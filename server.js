cefe0981708253fb101379a8412194ea7434814e
alishahwee <alisha.hwee@outlook.com>
2021-05-19 23:07:11 -0500

    Add get and post for short url

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// Global short url counter
+let urlCounter = 0;
+
+// In-memory "database"
+const shortUrls = {};
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +26,24 @@ app.get('/', function (req, res) {
 // Your first API endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Short URL get
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  res.redirect(shortUrls[req.params.shorturl]);
+});
+
+// Short URL post
+app.post('/api/shorturl', (req, res) => {
+  urlCounter++;
+
+  const jsonRes = {
+    original_url: req.body?.url,
+    short_url: urlCounter,
+  };
+
+  shortUrls[urlCounter] = jsonRes.original_url;
+  res.json(jsonRes);
 });
 
 app.listen(port, function () {
